c94e82e1459d84fbaa18a6dda38aa344bf214747
jeerim <jee.rim@datadoghq.com>
2018-10-01 14:18:53 -0400

    add setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='sketches',
+    version='0.1',
+    description='Distributed quantile sketches',
+    url='http://github.com/datadog/sketches-py',
+    author='Jee Rim, Charles-Philippe Masson',
+    author_email='jee.rim@datadoghq.com charles.masson@datadoghq.com',
+    license='BSD-3-Clause',
+    packages=['dogsketch', 'gkarray'],
+    install_requires=[
+        'numpy>=1.11.0'
+    ],
+)
