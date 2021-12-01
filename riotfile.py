348ad4216b5434c8ebfc215ed5690fceee28931e
kyle-verhoog <kyle@verhoog.ca>
2021-12-01 14:43:35 -0500

    Add support for Python 3.10

--- riotfile.py
+++ riotfile.py
@@ -8,7 +8,7 @@ venv = Venv(
         Venv(
             name="test",
             command="pytest {cmdargs}",
-            pys=["2.7", "3.6", "3.7", "3.8", "3.9"],
+            pys=["2.7", "3.6", "3.7", "3.8", "3.9", "3.10"],
             pkgs={
                 "pytest": latest,
                 "numpy": latest,
